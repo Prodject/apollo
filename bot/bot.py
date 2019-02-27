0ee321c9e8dba54db96ed4dfc1ddfce42b74133e
Adam Musciano <amusciano@gmail.com>
2019-02-26 19:07:47 -0500

    Added bot script

---
+++ bot/bot.py
@@ -0,0 +1,17 @@
+#Check for new songs on soundcloud user's profile every 15 seconds
+
+def send_to_c2():
+    pass
+
+def get_new_songs():
+    pass
+
+#Modules
+def get_passwords():
+    #run lazagne
+    pass
+
+def get_system_info():
+    pass
+
+
---
+++ bot/utils/readme.md
@@ -0,0 +1,3 @@
+This is a slightly modified version of this repository (https://github.com/danielcardeenas/AudioStego)
+
+Still uses the same arguments, and -f to decode a message
